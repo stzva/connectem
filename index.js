3644d1778de164c091d5525a1c03058d2d66dc27
Silvia Tzeneva <tzeneva@gmail.com>
2015-11-30 21:24:42 +0200

    Highlight of dots on click

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function Board(n) {
+function Board(n, boardId) {
 	this.p1 = [];
 	this.p2 = [];
 	this.inner = [];
 	this.outer = [];
 	this.n = n;
+	this.board = document.getElementById(boardId);
+	if (!this.board) {
+		throw new Error('No element with id ' + boardId + '.');
+	}
+	
 	var div;
 
 	for (var i = 0; i < n + 1; i++) {
@@ -43,36 +48,40 @@ function Board(n) {
 	}
 }
 
-Board.prototype.draw = function (boardId) {
-	var board = document.getElementById(boardId);
-
-	if (!board) {
-		throw new Error('No element with id ' + boardId + '.');
-	}
-	
+Board.prototype.draw = function () {	
 	//first row of p1
 	for (var i = 0; i < this.n; i++) {
-		board.appendChild(this.p1[0][i]);
+		this.board.appendChild(this.p1[0][i]);
 	}
 
 	for (var i = 0; i < this.n; i++) {
 		//p2 row with lines
 		for (var j = 0; j < this.n; j++) {
-			board.appendChild(this.p2[i][j]);
-			board.appendChild(this.outer[i][j]);
+			this.board.appendChild(this.p2[i][j]);
+			this.board.appendChild(this.outer[i][j]);
 		}
-		board.appendChild(this.p2[i][j]);
+		this.board.appendChild(this.p2[i][j]);
 		
 		//p1 row with lines
 		for (var j = 0; j < this.n - 1; j++) {
-			board.appendChild(this.p1[i + 1][j]);
+			this.board.appendChild(this.p1[i + 1][j]);
 			if (i < this.n - 1) {
-				board.appendChild(this.inner[i][j]);
+				this.board.appendChild(this.inner[i][j]);
 			}
 		}
-		board.appendChild(this.p1[i + 1][j]);
+		this.board.appendChild(this.p1[i + 1][j]);
 	}
 }
 
-var b = new Board(3);
-b.draw('board');
+Board.prototype.attachHandler = function () {
+	this.board.addEventListener('click', function(event) {
+		var target = event.target;
+		if(target.className && target.className.indexOf('dot') != -1) {
+			target.className += ' highlighted';
+		}
+	})
+}
+
+var b = new Board(3, 'board');
+b.draw();
+b.attachHandler();
