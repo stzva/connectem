7f4e7be72c16b0a8c60c277631bbd43a202e2d2d
Silvia Tzeneva <tzeneva@gmail.com>
2015-12-15 20:34:35 +0200

    Highlight possible moves

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function Board(n, boardId) {
 	if (!this.board) {
 		throw new Error('No element with id ' + boardId + '.');
 	}
-	
+	this.currentPlayer = 'p1';
+
 	var div;
 
 	for (var i = 0; i < n + 1; i++) {
@@ -74,12 +75,76 @@ Board.prototype.draw = function () {
 }
 
 Board.prototype.attachHandler = function () {
-	this.board.addEventListener('click', function(event) {
+	this.board.addEventListener('click', function (event) {
 		var target = event.target;
-		if(target.className && target.className.indexOf('dot') != -1) {
-			target.className += ' highlighted';
+		if (target.className &&
+			target.className.indexOf('dot') != -1 &&
+			target.className.indexOf(this.currentPlayer) != -1) {
+			this.highlightMoves(target);
+			this.nextTurn();
 		}
-	})
+	}.bind(this));
+}
+
+Board.prototype.getTargetCoords = function(target) {
+	for (var i = 0; i < this[this.currentPlayer].length; i++) {
+		for (var j = 0; j < this[this.currentPlayer][i].length; j++) {
+			if (this[this.currentPlayer][i][j] === target) {
+				return [i, j];
+			}
+		}
+	}
+};
+
+Board.prototype.highlightMoves = function(target) {
+	var targetCoords = this.getTargetCoords(target);
+	var targetX = targetCoords[0];
+	var targetY = targetCoords[1];
+	var neighbours = [
+		[-1, 0],
+		[0, 1],
+		[1, 0],
+		[0, -1]
+	];
+	var possibleMoves = {
+		p1: [
+			[-1, 0, 'outer'],
+			[-1, 0, 'inner'],
+			[0, 0, 'outer'],
+			[-1, -1, 'inner']
+		],
+		p2: [
+			[-1, -1, 'inner'],
+			[0, 0, 'outer'],
+			[0, -1, 'inner'],
+			[0, -1, 'outer']
+		]
+	};
+	
+	var playerMatrix = this[this.currentPlayer];
+	for (var i = 0; i < 4; i++) {
+		var movesRow = possibleMoves[this.currentPlayer][i];
+		
+		var lineMatrix = this[movesRow[2]];
+		var lineX = targetX + movesRow[0];
+		var lineY = targetY + movesRow[1];
+		var line = lineMatrix[lineX] && lineMatrix[lineX][lineY];
+		
+		if (line && line.childNodes.length === 0) {
+			var neighbourX = targetX + neighbours[i][0];
+			var neighbourY = targetY + neighbours[i][1];
+			var neighbour = playerMatrix[neighbourX] && playerMatrix[neighbourX][neighbourY];
+			if (neighbour) {
+				neighbour.className += ' highlighted';
+			}
+		}
+	}
+}
+
+Board.prototype.nextTurn = function () {
+	var otherPlayer = (this.currentPlayer == 'p1') ? 'p2' : 'p1';
+
+	this.currentPlayer = otherPlayer;
 }
 
 var b = new Board(3, 'board');
