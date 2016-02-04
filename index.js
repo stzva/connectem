5f83fd938e2a57e770d90a6f4e5e917e6f801071
Silvia Tzeneva <tzeneva@gmail.com>
2016-02-04 20:05:36 +0200

    Check for winner

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ Board.prototype.attachHandler = function () {
         if (target.className &&
             target.className.indexOf('line') != -1 &&
             target.className.indexOf('taken') == -1) {
-                this.makeMove(target);
+            this.makeMove(target);
         }
     }.bind(this));
 }
@@ -106,16 +106,94 @@ Board.prototype.makeMove = function (target) {
     target.className += ' taken';
     target.children[0].className += ' ' + this.currentPlayer;
     target.children[1].className += ' ' + this.currentPlayer;
-    this.nextTurn();    
+    this.nextTurn();
+}
+
+Board.prototype.isWinner = function () {
+    var queue = [];
+    var visited = [];
+    for (var i = 0; i < this.n + 1; i++) {
+        visited[i] = [];
+        for (var j = 0; j < this.n + 1; j++) {
+            visited[i].push(false);
+        }
+    }
+    for (var i = 0; i < this.n; i++) {
+        if (this.currentPlayer == 'p1') {
+            queue.push({ x: 0, y: i });
+            visited[0][i] = true;
+        }
+        else {
+            queue.push({ x: i, y: 0 });
+            visited[i][0] = true;
+        }
+    }
+
+    while (queue.length > 0) {
+        //console.log(this.currentPlayer, ': ', queue.map(a => `[${a.x},${a.y}]`).join(','));
+        var vert = queue.shift();
+
+
+        if ((this.currentPlayer == 'p1' && vert.x == this.n)
+            || (this.currentPlayer == 'p2' && vert.y == this.n)) {
+            return true;
+        }
+
+        var neighbours = [
+            [-1, 0],
+            [0, 1],
+            [1, 0],
+            [0, -1]
+        ];
+        var edgeDeltas = {
+            p1: [
+                [-1, 0, 'outer'],
+                [-1, 0, 'inner'],
+                [0, 0, 'outer'],
+                [-1, -1, 'inner']
+            ],
+            p2: [
+                [-1, -1, 'inner'],
+                [0, 0, 'outer'],
+                [0, -1, 'inner'],
+                [0, -1, 'outer']
+            ]
+        };
+        for (var i = 0; i < neighbours.length; i++) {
+            var x = vert.x + neighbours[i][0];
+            var y = vert.y + neighbours[i][1];
+
+            if (((this.currentPlayer == 'p1' && x >= 0 && x <= this.n && y >= 0 && y <= this.n + 1)
+                || (this.currentPlayer == 'p2' && x >= 0 && x <= this.n + 1 && y >= 0 && y <= this.n))
+                && !visited[x][y]) {
+                var movesRow = edgeDeltas[this.currentPlayer][i];
+
+                var lineMatrixName = movesRow[2];
+                var lineMatrix = this[lineMatrixName];
+                var lineX = vert.x + movesRow[0];
+                var lineY = vert.y + movesRow[1];
+                if (lineMatrix[lineX] && lineMatrix[lineX][lineY] && lineMatrix[lineX][lineY].className.indexOf('taken') != -1
+                    && lineMatrix[lineX][lineY].children[0].className.indexOf(this.currentPlayer) != -1) {
+                    queue.push({ x: x, y: y });
+                    visited[x][y] = true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 Board.prototype.nextTurn = function () {
+    if (this.isWinner()) {
+        alert((this.currentPlayer == 'p1' ? 'Red' : 'Green') + " wins!");
+        return;
+    }
     var otherPlayer = (this.currentPlayer == 'p1') ? 'p2' : 'p1';
     this.currentPlayer = otherPlayer;
     this.board.className = this.currentPlayer;
 }
 
-var b = new Board(4, 'board');
+var b = new Board(3, 'board');
 b.draw();
 b.nextTurn();
 b.attachHandler();
